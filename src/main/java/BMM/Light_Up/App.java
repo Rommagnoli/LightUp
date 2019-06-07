7c7a62f9292af0bedb6989e8e13d11a43429e7e3
Rommagnoli <roman_mag1993@hotmail.com>
2019-06-07 15:16:30 -0300

    Creación del proyecto LightUp

---
+++ src/main/java/BMM/Light_Up/App.java
@@ -0,0 +1,13 @@
+package BMM.Light_Up;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
