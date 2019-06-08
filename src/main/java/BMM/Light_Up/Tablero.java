28f2f6f8f52722265bff7b769c36fb14585f3bf9
Rommagnoli <roman_mag1993@hotmail.com>
2019-06-08 01:42:47 -0300

    Cambio de nombre a la clase Tablero

--- src/main/java/BMM/Light_Up/Tablero.java
+++ src/main/java/BMM/Light_Up/Tablero.java
@@ -1,19 +1,28 @@
 package BMM.Light_Up;
 
 
-public class Board {
+public class Tablero {
 
-	private static final int FIL = 7;
-	private static final int COL = 7;
+	private static final Integer FIL = 7;
+	private static final Integer COL = 7;
 	private static Celdas[][] elementos;
 
-	public Board () {
-		this.elementos = new Celdas[FIL][COL];
-		for (int filas = 0; filas < FIL; filas++) {
-			for(int columnas = 0; columnas < COL; columnas++) {
-				Celdas celda = new Celdas();
-				this.elementos [FIL][COL] = celda;
+	public Tablero() {
+			this.elementos = new Celdas[FIL][COL];
+			for (Integer filas = 0; filas < FIL; filas++) {
+				for(Integer columnas = 0; columnas < COL; columnas++) {
+					Celdas celda = new Celdas();
+					this.elementos [FIL][COL] = celda;
+				}
 			}
-		}
+			
+			elementos [3][1].setCeldaNegra();
+			elementos [6][4].setCeldaNegra();		
+			elementos [0][2].setCeldaNegra(1);
+			elementos [1][3].setCeldaNegra(1);
+			elementos [4][0].setCeldaNegra(3);
+			elementos [5][3].setCeldaNegra(0);
+			elementos [3][5].setCeldaNegra(2);
+			elementos [2][6].setCeldaNegra(3);
 	}
 }
