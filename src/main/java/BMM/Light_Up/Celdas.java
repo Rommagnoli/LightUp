3c01223716235f2a893e701d311bbdff96b968db
boaglio15 <agustinboaglio.95@gmail.com>
2019-06-08 00:47:59 -0300

    Modificacion pom.xml y agregacion metodos en la clase celdas

--- src/main/java/BMM/Light_Up/Celdas.java
+++ src/main/java/BMM/Light_Up/Celdas.java
@@ -2,4 +2,43 @@ package BMM.Light_Up;
 
 public class Celdas {
 
+    private Integer valorCelda;
+    private boolean esNegra;
+    private boolean esLuz;
+
+    public Celdas(){
+        valorCelda = null;
+        esNegra = false;
+        esLuz = false;
+    }
+
+    public void setCeldaNegra(){
+        this.valorCelda = null;
+        this.esNegra = true;
+        this.esLuz = false;
+    }
+
+    public void setCeldaNegra(int valor){
+        this.valorCelda = valor;
+        this.esNegra = true;
+        this.esLuz = false;
+    }
+
+    public void setCeldaLuz(){
+        this.valorCelda = null;
+        this.esNegra = false;
+        this.esLuz = true;
+    }
+
+    public Integer getValorCelda(){
+        return this.valorCelda;
+    }
+
+    public boolean esNegra(){
+        return this.esNegra;
+    }
+
+    public boolean esLuz(){
+        return this.esLuz;
+    }
 }
