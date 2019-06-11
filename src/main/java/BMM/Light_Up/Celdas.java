1d602215c1741259142dbd6bf6665690bb0b10cb
Rommagnoli <roman_mag1993@hotmail.com>
2019-06-11 00:02:17 -0300

    Agregado de JavaDocs y correcciones menores

--- src/main/java/BMM/Light_Up/Celdas.java
+++ src/main/java/BMM/Light_Up/Celdas.java
@@ -1,44 +1,113 @@
 package BMM.Light_Up;
 
+/**
+ * Clase que representa las celdas que son contenidas en el tablero
+ *
+ */
 public class Celdas {
 
+    /**
+     * Contiene el valor que indica la cantidad de lamparas que debe haber
+     * alrededor de una casilla negra
+     */
     private Integer valorCelda;
+    
+    /**
+     * Informa si una casilla es negra
+     */
     private boolean esNegra;
+    
+    /**
+     * Informa si en una celda hay una lampara
+     */
+    private boolean esLampara;
+    
+    /**
+     * Informa si una casilla esta iluminada por una lampara
+     */
     private boolean esLuz;
 
-    public Celdas(){
+    /**
+     * Constructor que setea una celda completamente vacia
+     */
+    public Celdas() {
         valorCelda = null;
         esNegra = false;
         esLuz = false;
+        esLampara = false;
     }
 
-    public void setCeldaNegra(){
-        this.valorCelda = null;
+    /**
+     * Setea una celda vacia como una celda negra
+     */
+    public void setCeldaNegra() {
         this.esNegra = true;
-        this.esLuz = false;
     }
 
-    public void setCeldaNegra(int valor){
+    /**
+     * Setea una celda negra con un valor especifico
+     * 
+     * @param valor es el que indica la cantidad de lamparas que tiene 
+     * que haber a su alrededor
+     * 
+     * @throws IllegalArgumentException si el valor es inferior a cero o mayor que 4
+     */
+    public void setCeldaNegra(int valor) throws IllegalArgumentException{
+        if ((valor < 0) || (valor > 4)) {
+            throw new IllegalArgumentException ("El valor de una celda negra debe ser mayor o igual que cero y menor o igual que 4");
+        }
         this.valorCelda = valor;
         this.esNegra = true;
-        this.esLuz = false;
+    }
+    
+    /**
+     * Setea en una celda, una lampara
+     */
+    public void setLampara() {
+        this.esLampara = true;
     }
 
-    public void setCeldaLuz(){
-        this.valorCelda = null;
-        this.esNegra = false;
+    /**
+     * Iluminia una celda si esta es alcanzada por una lampara
+     */
+    public void setCeldaLuz() {
         this.esLuz = true;
     }
 
-    public Integer getValorCelda(){
+    /**
+     * Informa el valor de una celda negra
+     * 
+     * @return valorCelda con el numero de lamparas que deben encontrarse alrededor
+     * de la celda
+     */
+    public Integer getValorCelda() {
         return this.valorCelda;
     }
 
-    public boolean esNegra(){
+    /**
+     * Informa si una celda es negra
+     * 
+     * @return true si la celda es negra
+     */
+    public boolean esNegra() {
         return this.esNegra;
     }
 
-    public boolean esLuz(){
+    /**
+     * Informa si una celda esta iluminada
+     * 
+     * @return true si la celda es alcanzada por una lampara
+     */
+    public boolean esLuz() {
         return this.esLuz;
+    }
+    
+    /**
+     * Informa si una celda contiene una lampra
+     * 
+     * @return true si la celda tiene una lampara
+     */
+    public boolean esLampara() {
+        return this.esLampara;
     }
 }
