3e012e74641e521267b78defad4d5e6e18f90b0f
Rommagnoli <roman_mag1993@hotmail.com>
2019-06-13 21:32:04 -0300

    Restructurado de la clase Tablero

--- src/main/java/BMM/Light_Up/Tablero.java
+++ src/main/java/BMM/Light_Up/Tablero.java
@@ -1,274 +1,300 @@
 package BMM.Light_Up;
 
 /**
- * Clase que repersenta el tablero de juego
+ * Clase que repersenta el tablero de juego.
  *
  */
 public class Tablero {
-    
-    /**
-     * Asigna el numero de Filas del tablero.
-     */
-	private static final Integer FIL = 7;
+  
+  /**
+   * Asigna el numero maximo del arreglo.
+   */
+  private static final Integer MAX = 49;
+  
+  /**
+   * Arreglo en el que se guarda el tablero de juego.
+   */
+  private Celdas[] elementos;
 	
-	/**
-	 * Asigna el numero de Columas del tablero.
-	 */
-	private static final Integer COL = 7;
-	
-	/**
-	 * Matriz en la que se guarda el tablero de juego.
-	 */
-	private Celdas[][] elementos;
-	
-	/**
-	 * Representa la cantidad de celdas con luz en la matriz. 
-	 */
-	private int cantCeldasLuz;
-	
-	/**
-	 * Representa la cantidad de celdas negras en la matriz
-	 */
-	private int cantCeldasNegras;
+  /**
+   * Representa la cantidad de celdas con luz en la matriz. 
+   */
+  private int cantCeldasLuz;
 
-	/**
-	 * Crea un tablero predeterminado de 7 x 7 
-	 * para el juego Light Up
-	 */
-	public Tablero() {
-        this.elementos = new Celdas[FIL][COL];
-        this.cantCeldasLuz = 0;
-		for (Integer filas = 0; filas < FIL; filas++) {
-            for(Integer columnas = 0; columnas < COL; columnas++) {
-                Celdas celda = new Celdas();
-				this.elementos [FIL][COL] = celda;
-			}
-		}
-			
-		/*elementos [3][1].setCeldaNegra();
+  /**
+   * Representa la cantidad de celdas negras en la matriz.
+   */
+  private int cantCeldasNegras;
+
+  /**
+   * Crea un tablero predeterminado de 7 x 7 para el juego Light Up.
+   */
+  public Tablero() {
+    this.elementos = new Celdas[MAX];
+    this.cantCeldasLuz = 0;
+    for (Integer tabIndex = 0; tabIndex < MAX; tabIndex++) {
+      Celdas celda = new Celdas();
+      this.elementos [tabIndex] = celda;
+    }
+  }
+
+  /*
+		elementos [3][1].setCeldaNegra();
 		elementos [6][4].setCeldaNegra();		
 		elementos [0][2].setCeldaNegra(1);
 		elementos [1][3].setCeldaNegra(1);
 		elementos [4][0].setCeldaNegra(3);
 		elementos [5][3].setCeldaNegra(0);
 		elementos [3][5].setCeldaNegra(2);
-		elementos [2][6].setCeldaNegra(3);*/
+		elementos [2][6].setCeldaNegra(3);
+  */
+
+  /**
+   * Funcion que retorna la matriz de celdas.
+   * @return la matriz que simboliza el tablero.
+   */
+  public Celdas[] getTablero(){
+    return elementos;
+  }
+
+  /**
+   * Funcion que dada la posiscion en un arreglo, devuelve la columna correspondiente
+   * de la matriz donde se encontraria.
+   * @param tabIndex es el indice del arreglo
+   * @return el indice de la columna a la que hace referencia la posicion
+   */
+  private static int transformarColumna(int tabIndex) {
+    int col = tabIndex;
+    while (col > 8) {
+      col = col - 8;
+    }
+    return col;
+  }
+
+  /**
+   * Funcion que dice si un tablero no es null.
+   * @return true si es valido, falso si es invalido.
+   */
+  public boolean tableroValido() {
+    return (this.getTablero() != null);
+  }
+
+  /**
+   * Funcion que retorna la cantidad de celdas con luz.
+   * @return cantidad de celdas con luz.
+   */
+  public int getCantCeldasLuz() {
+    return (this.cantCeldasLuz);
+  }
+
+  /**
+   * Funcion que retorna la cantidad de celdas negras.
+   * @return la cantidad de celdas negras del tablero.
+   */
+  public int getCantCeldasNegras() {
+    return this.cantCeldasNegras;
+  }
+
+  /**
+   * Funcion que retorna la cantidad de celdas sin luz del tablero.
+   * @return cantidad de celdas sin luz.
+   */
+  public int getCantidadSinLuz() {
+    return (MAX - this.cantCeldasNegras - this.cantCeldasLuz);
+  }
+
+  /**
+   * Funcion que setea una celda negra en la matriz si la posicion es valida.
+   * @param tabIndex es el indice de movimiento para el arreglo.
+   */
+  public void nuevaCeldaNegra(int tabIndex) {
+    if  (this.tableroValido()) {
+      throw new IllegalArgumentException("El tablero es nulo");
+    }
+    
+    if (tabIndex < 0 || tabIndex > 0) {
+      throw new IllegalArgumentException("Posiciones invalidas");
+    }  
+    
+    if (this.elementos[tabIndex].esNegra()) {
+      throw new IllegalArgumentException("La celda en la posicion " + tabIndex + "ya es negra");
+    }
+
+    this.elementos[tabIndex].setCeldaNegra();
+    this.cantCeldasNegras++;
+  }
+
+  /**
+   * Funcion que setea una celda negra con valor en la matriz si la posicion es valida.
+   * @param tabIndex es el indice de movimiento para el arreglo.
+   * @param valor es el valor que tendra la celda negra.
+   */
+  public void nuevaCeldaNegra(int tabIndex, Integer valor) {
+    if (this.tableroValido()) {
+      throw new IllegalArgumentException("El tablero es nulo");
+    }
+
+    if (tabIndex < 0 || tabIndex > MAX) {
+      throw new IllegalArgumentException("Posiciones invalidas");
+    }
+
+    if (this.elementos[tabIndex].esNegra()) {
+      throw new IllegalArgumentException("La celda en la posicion " + tabIndex + "ya es negra");
+    }
+
+    this.elementos[tabIndex].setCeldaNegra(valor);
+    this.cantCeldasNegras++;
+  }
+
+  /**
+   * Funcion que dice si poner una lampara en la posicion es valida 
+   * teniendo una celda negra con valor adyacente.
+   * @param tabIndex es el indice de movimiento para el arreglo.
+   * @return true si la posicion es valida, falso si no lo es.
+   */
+  public boolean adyCeldaNegra(int tabIndex) {
+    int valCelda = 0;
+    int cantLamparasCelda = 0;
+
+    if ((tabIndex - 7 >= 0) && this.elementos[tabIndex - 7].esNegraValor()) {      // Elemento de arriba (representacion en matriz)
+      valCelda = this.elementos[tabIndex - 7].getValorCelda();
+      cantLamparasCelda = this.cantLamparasAdy(tabIndex - 7);
+      if (valCelda >= cantLamparasCelda) {
+        return false;
+      }
 	}
+    if ((tabIndex + 7 < MAX) && this.elementos[tabIndex + 7].esNegraValor()) {     // Elemento de abajo (representacion en matriz)
+      valCelda = this.elementos[tabIndex + 7].getValorCelda();
+      cantLamparasCelda = this.cantLamparasAdy(tabIndex + 7);
+      if (valCelda >= cantLamparasCelda) {
+        return false;
+      }
+	}
+	if ((tabIndex - 1 >= 0) && this.elementos[tabIndex - 1].esNegraValor()) {      // Elemento de la Izquierda (representacion en matriz)
+	  valCelda = this.elementos[tabIndex - 1].getValorCelda();
+	  cantLamparasCelda = this.cantLamparasAdy(tabIndex - 1);
+	  if (valCelda >= cantLamparasCelda) {
+        return false;
+      }
+	}
+	if ((tabIndex + 1 < MAX) && this.elementos[tabIndex + 1].esNegraValor()) {     // Elemento de la derecha (representacion en matriz)
+	  valCelda = this.elementos[tabIndex + 1].getValorCelda();
+	  cantLamparasCelda = this.cantLamparasAdy(tabIndex + 1);
+	  if (valCelda >= cantLamparasCelda) {
+        return false;
+      }
+	}
+	return true;
+  }
 	
-	/**
-	 * Funcion que retorna la matriz de celdas.
-	 * @return la matriz que simboliza el tablero.
-	 */
-	public Celdas[][] getTablero(){
-		return elementos;
+  /**
+   * Funcion que dice si una posicion es valida para poner una lampara en la matriz.
+   * @param tabIndex es el indice de movimiento para el arreglo.
+   * @return true si es valida la posicion, false si es invalida.
+   */
+  public boolean posValidaNuevaLamp(int tabIndex) {
+    if (this.tableroValido()) {
+      throw new IllegalArgumentException("El tablero es nulo");
+    }
+	if (tabIndex < 0 || tabIndex > MAX) {
+	  throw new IllegalArgumentException("Posiciones invalidas");
 	}
+	if (this.elementos[tabIndex].esNegra()) {
+      return false;
+    } else {
+	  if (this.elementos[tabIndex].esLuz()) {
+        return false;
+      } else {
+	    if (!(this.adyCeldaNegra(tabIndex))) {
+          return false;
+        } else {
+          return true;
+        }
+	  }
+	}
+  }
 	
-	/**
-	 * Funcion que dice si un tablero no es null.
-	 * @return true si es valido, falso si es invalido.
-	 */
-	public boolean tableroValido() {
-		return (this.getTablero() != null);
+  /**
+   * Funcion que setea en la matriz una lampara si y solo si la posicion es valida.
+   * @param tabIndex es el indice de movimiento para el arreglo.
+   */
+  public void nuevaLampara(int tabIndex) {
+    if (!(this.posValidaNuevaLamp(tabIndex))) {
+      throw new IllegalArgumentException("Posicion invalida para una lampara");
+    } else {
+	  this.elementos[tabIndex].setLampara();
+	  this.cantCeldasLuz++;
+	  
+	  int i = tabIndex - 7;
+	  while ((i>=0) && (!(this.elementos[i].esNegra()))) {         //Illuminar el tablero hacia arriba
+	    if (this.elementos[i].esLuz()) {
+          i = i - 7;
+        } else {
+          this.elementos[i].setCeldaLuz();
+          this.cantCeldasLuz++;
+          i = i - 7;
+        }		
+	  }
+	  
+	  i = tabIndex + 7;
+	  while ((i < 7) && (!(this.elementos[i].esNegra()))){         //Illuminar el tablero hacia abajo
+	    if (this.elementos[i].esLuz()) {
+          i = i + 7;
+        } else {
+          this.elementos[i].setCeldaLuz();
+          this.cantCeldasLuz++;
+          i = i + 7;
+        }
+	  }
+	  
+	  i = tabIndex - 1;
+	  int col = (Tablero.transformarColumna(tabIndex)) - 1;
+	  while ((col >= 0) && ((!this.elementos[i].esNegra()))) {     //Illuminar el tablero hacia la izquierda
+	    if (this.elementos[i].esLuz()) {
+          i--;
+        } else {
+          this.elementos[i].setCeldaLuz();
+          this.cantCeldasLuz++;
+          i--;
+        }
+	    col--;
+	  }
+	  
+	  col = (Tablero.transformarColumna(tabIndex)) + 1;
+	  i = tabIndex + 1;
+	  while ((col < 8) && (!(this.elementos[i].esNegra()))) {      //Illuminar el tablero hacia la derecha
+	    if (this.elementos[i].esLuz()){
+          i++;
+        } else {
+		  this.elementos[i].setCeldaLuz();
+		  this.cantCeldasLuz++;
+		  i++;
+		}
+	    col++;
+	  }
 	}
+  }
 	
-	/**
-	 * Funcion que retorna la cantidad de celdas con luz.
-	 * @return cantidad de celdas con luz.
-	 */
-	public int getCantCeldasLuz() {
-		return (this.cantCeldasLuz);
+  /**
+   * Funcion privada que dice la cantidad de lamaparas que 
+   * tiene adyacente una celda negra con valor.
+   * @param tabIndex representa el indice del arregla.
+   * @return cantidad de lamparas adyacentes.
+   */
+  private int cantLamparasAdy(int tabIndex) {
+    int cantLamparas = 0;
+    if (tabIndex - 7 >= 0 && (this.elementos[tabIndex - 7].esLampara())) {
+      cantLamparas++;
+    }
+	if (tabIndex + 7 < MAX && (this.elementos[tabIndex + 7].esLampara())) {
+	  cantLamparas++;
 	}
-	
-	/**
-	 * Funcion que retorna la cantidad de celdas negras
-	 * @return la cantidad de celdas negras del tablero
-	 */
-	public int getCantCeldasNegras() {
-	    return this.cantCeldasNegras;
+	if (tabIndex - 1 >= 0 && (this.elementos[tabIndex - 1].esLampara())) {
+	  cantLamparas++;
 	}
-	
-	/**
-	 * Funcion que retorna la cantidad de celdas sin luz del tablero.
-	 * @return cantidad de celdas sin luz
-	 */
-	public int getCantidadSinLuz() {
-	    return ((FIL*COL) - this.cantCeldasNegras - this.cantCeldasLuz);
+	if (tabIndex + 1 < MAX && (this.elementos[tabIndex + 1].esLampara())) {
+	  cantLamparas++;
 	}
-	
-	/**
-	 * Funcion que setea una celda negra en la matriz si la posicion es valida.
-	 * @param posFila es el indice que representa una fila en la matriz.
-	 * @param posCol es el indice que representa una columna en la matriz.
-	 */
-	public void nuevaCeldaNegra(int posFila, int posCol) {
-		if (this.tableroValido())
-			throw new IllegalArgumentException("El tablero es nulo");
-		if ((posFila >= FIL || posFila < 0) || (posCol <= 0 || posCol >= COL)) {
-			throw new IllegalArgumentException("Posiciones invalidas");
-		}
-		if (this.elementos[posFila][posCol].esNegra())
-			throw new IllegalArgumentException("La celda en la posicion " + posFila + posCol + "ya es negra");
-		this.elementos[posFila][posCol].setCeldaNegra(); 
-		this.cantCeldasNegras++;
-	}
-	
-	/**
-	 * Funcion que setea una celda negra con valor en la matriz si la posicion es valida
-	 * @param posFila es el indice que representa una fila en la matriz.
-	 * @param posCol es el indice que representa una columna en la matriz.
-	 * @param valor es el valor que tendra la celda negra.
-	 */
-	public void nuevaCeldaNegra(int posFila, int posCol, Integer valor) {
-		if (this.tableroValido())
-			throw new IllegalArgumentException("El tablero es nulo");
-		if ((posFila >= FIL || posFila < 0) || (posCol <= 0 || posCol >= COL)) {
-			throw new IllegalArgumentException("Posiciones invalidas");
-		}
-		if (this.elementos[posFila][posCol].esNegra())
-			throw new IllegalArgumentException("La celda en la posicion " + posFila + posCol + "ya es negra");
-		this.elementos[posFila][posCol].setCeldaNegra(valor);
-		this.cantCeldasNegras++;
-	}
-	
-	/**
-	 * Funcion que dice si poner una lampara en la posicion es valida 
-	 * teniendo una celda negra con valor adyacente.
-	 * @param posFila indica la fila en una matriz.
-	 * @param posCol indica la columna en una matriz.
-	 * @return true si la posicion es valida, falso si no lo es.
-	 */
-	public boolean adyCeldaNegra(int posFila, int posCol) {
-		int valCelda = 0;
-		int cantLamparasCelda = 0;
-		
-		if ((posFila - 1 >= 0) && this.elementos[posFila - 1][posCol].esNegraValor()) {
-			valCelda = this.elementos[posFila - 1][posCol].getValorCelda();
-			cantLamparasCelda = this.cantLamparasAdy(posFila-1, posCol);
-			if (valCelda >= cantLamparasCelda)
-				return false;
-		}
-		if ((posFila + 1 < FIL) && this.elementos[posFila + 1][posCol].esNegraValor()) {
-			valCelda = this.elementos[posFila + 1][posCol].getValorCelda();
-			cantLamparasCelda = this.cantLamparasAdy(posFila + 1, posCol);
-			if (valCelda >= cantLamparasCelda)
-				return false;
-		}
-		if ((posCol - 1 >= 0) && this.elementos[posFila][posCol - 1].esNegraValor()) {
-			valCelda = this.elementos[posFila][posCol - 1].getValorCelda();
-			cantLamparasCelda = this.cantLamparasAdy(posFila, posCol - 1);
-			if (valCelda >= cantLamparasCelda)
-				return false;
-		}
-		if ((posCol + 1 < COL) && this.elementos[posFila][posCol + 1].esNegraValor()) {
-			valCelda = this.elementos[posFila][posCol + 1].getValorCelda();
-			cantLamparasCelda = this.cantLamparasAdy(posFila, posCol + 1);
-			if (valCelda >= cantLamparasCelda)
-				return false;
-		}
-		return true;
-	}
-	
-	/**
-	 * Funcion que dice si una posicion es valida para poner una lampara en la matriz.
-	 * @param posFila es el indice que representa una fila en la matriz.
-	 * @param posCol es el indice que representa una columna en la matriz.
-	 * @return true si es valida la posicion, false si es invalida.
-	 */
-	public boolean posValidaNuevaLamp(int posFila, int posCol) {
-		if (this.tableroValido())
-			throw new IllegalArgumentException("El tablero es nulo");
-		if ((posFila >= FIL || posFila < 0) || (posCol <= 0 || posCol >= COL)) {
-			throw new IllegalArgumentException("Posiciones invalidas");
-		}
-		if (this.elementos[posFila][posCol].esNegra())
-			return false;
-		else {
-			if (this.elementos[posFila][posCol].esLuz())
-				return false;
-			else {
-				if (!(this.adyCeldaNegra(posFila, posCol)))
-					return false;
-				else
-					return true;
-			}
-		}
-	}
-	
-	/**
-	 * Funcion que setea en la matriz una lampara si y solo si la posicion es valida.
-	 * @param posFila es el indice que representa una fila en la matriz.
-	 * @param posCol es el indice que representa una columna en la matriz.
-	 */
-	public void nuevaLampara(int posFila, int posCol) {
-		if (!(this.posValidaNuevaLamp(posFila, posCol)))
-			throw new IllegalArgumentException("Posicion invalida para una lampara");
-		else {
-			this.elementos[posFila][posCol].setLampara();
-			this.cantCeldasLuz++;
-			int i = posFila - 1;
-			while ((i>=0) && (!(this.elementos[i][posCol].esNegra()))) {
-				if (this.elementos[i][posCol].esLuz())
-					i--;
-				else {
-					this.elementos[i][posCol].setCeldaLuz();
-					this.cantCeldasLuz++;
-					i--;
-				}		
-			}
-			i = posFila + 1;
-			while ((i<FIL) && (!(this.elementos[i][posCol].esNegra()))){
-				if (this.elementos[i][posCol].esLuz())
-					i++;
-				else {
-					this.elementos[i][posCol].setCeldaLuz();
-					this.cantCeldasLuz++;
-					i++;
-				}
-			}
-			i = posCol - 1;
-			while ((i>=0) && ((!this.elementos[posFila][i].esNegra()))) {
-				if (this.elementos[posFila][i].esLuz())
-					i--;
-				else {
-					this.elementos[posFila][i].setCeldaLuz();
-					this.cantCeldasLuz++;
-					i--;
-				}
-			}
-			i = posCol + 1;
-			while ((i<COL) && (!(this.elementos[posFila][i].esNegra()))) {
-				if (this.elementos[posFila][i].esLuz())
-					i++;
-				else {
-					this.elementos[posFila][i].setCeldaLuz();
-					this.cantCeldasLuz++;
-					i++;
-				}
-			}
-		}
-	}
-	
-	/**
-	 * Funcion privada que dice la cantidad de lamaparas que 
-	 * tiene adyacente una celda negra con valor
-	 * @param posFila representa la fila de una matriz.
-	 * @param posCol representa la columna de una matriz.
-	 * @return cantidad de lamparas adyacentes.
-	 */
-	private int cantLamparasAdy(int posFila, int posCol) {
-		int cantLamparas = 0;
-		if (posFila-1>=0 && (this.elementos[posFila-1][posCol].esLampara())) {
-			cantLamparas++;
-		}
-		if (posFila+1<FIL && (this.elementos[posFila+1][posCol].esLampara())) {
-			cantLamparas++;
-		}
-		if (posCol-1>=0 && (this.elementos[posFila][posCol-1].esLampara())) {
-			cantLamparas++;
-		}
-		if (posCol+1<COL && (this.elementos[posFila][posCol+1].esLampara())) {
-			cantLamparas++;
-		}
-		return cantLamparas;
-	}
-}	
+	return cantLamparas;
+  }
+}
