d3b6a07b3eda75973d585739c2cc8de11ab4ba1f
boaglio15 <agustinboaglio.95@gmail.com>
2019-06-16 19:40:20 -0300

    Modificacion FitnesFunction y agragacion metodo posCeldasNegrasValor en Tablero

--- src/main/java/BMM/Light_Up/Fitness.java
+++ src/main/java/BMM/Light_Up/Fitness.java
@@ -1,45 +1,59 @@
 package BMM.Light_Up;
 
+import java.util.*;
 import org.jgap.*;
 
-public class Fitness extends FitnessFunction{
-	
-	/**
-	 * Variable que lleva el tablero original. 
-	 */
-	private Tablero tablero;
-	
-	/**
-	 * Constructor de la clase Fitness.
-	 * @param t representa el tablero origiinal con las celdas negras.
-	 */
-	public Fitness(Tablero t) {
-		tablero = t;
-	}
-	
-	/**
-	 * Funcion que evalua que tan apto es un Cromosoma
-	 * @param individuo representa un cromosoma.
-	 * @return un valor el cual dice que tan apto es el individuo, si es apto retorna un numero
-	 * muy alto de aptitud.
-	 */
-	public double evaluate(LightUpChromosome individuo) {
-		double res = 0;
-		int posicion = 0;
-		for (Gene gen : individuo.getGenes()) {
-			Integer lamparaONo = (Integer) gen.getAllele();
-			if (lamparaONo == 1) {
-				try {
-					tablero.nuevaLampara(posicion);
-				}
-				catch(IllegalArgumentException excepcion) {
-					res = res - 500;
-				}
-				res = res + 500;
-			}
-			posicion++;
-		}
-		return res;	
-	}
-	
+public class Fitness extends FitnessFunction {
+
+    private static final long serialVersionUID = 1L;
+    
+    /**
+     * Variable que lleva el tablero original.
+     */
+    private Tablero tablero;
+
+    /**
+     * Constructor de la clase Fitness.
+     *
+     * @param t representa el tablero origiinal con las celdas negras.
+     */
+    public Fitness(Tablero t) {
+        tablero = t;
+    }
+
+    /**
+     * Funcion que evalua que tan apto es un Cromosoma
+     *
+     * @param individuo representa un cromosoma.
+     * @return un valor el cual dice que tan apto es el individuo, si es apto
+     * retorna un numero muy alto de aptitud.
+     */
+    public double evaluate(IChromosome individuo) {
+        double res = 0;
+        int posicion = 0;
+        ArrayList<Integer> posCeldasNegras = Tablero.posCeldasNegrasValor(tablero);
+        Boolean isCatch = false;
+        for (Gene gen : individuo.getGenes()) {
+            Integer lamparaONo = (Integer) gen.getAllele();
+            if (lamparaONo == 1) {
+                try {
+                    tablero.nuevaLampara(posicion);
+                } catch (IllegalArgumentException excepcion) {
+                    isCatch = true;
+                }
+                if (!isCatch){
+                    res = res + 1;
+                    isCatch = false;
+                }
+            }
+            posicion++;
+        }
+        for(Integer pos : posCeldasNegras){
+            if (tablero.getTablero()[pos].getValorCelda() == tablero.cantLamparasAdy(pos)){
+                res = res + 200;
+            }    
+        }
+        return res;
+    }
+
 }
